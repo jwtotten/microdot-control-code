a3c8e93ec01337187d9db6b853ead1334c599003
Jack <jacktotten@hotmail.co.uk>
2026-08-19 20:59:13 +0100

    Microdot class
    Created a microdot class and implemented how i want the homing of the legs to occur at startup.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 
 from controllers import LegController
 
+class Microdot:
+    def __init__(self):
+        # Initialize the microdot with 4 leg controllers
+        self.legs = [LegController(leg_id=i) for i in range(4)]
+
+    def get_leg(self, leg_id):
+        if 0 <= leg_id < len(self.legs):
+            return self.legs[leg_id]
+        else:
+            raise ValueError("Invalid leg ID")
+
 if __name__ == "__main__":
-    leg_controller = LegController()
+    microdot = Microdot()
+    # home all of the legs at startup
+    for leg in microdot.legs:
+        leg.home()
 
     
