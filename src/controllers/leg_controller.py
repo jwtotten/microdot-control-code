8c7a942863d7a13d1aa1a87f3a83cd85e0cf71d6
Jack <jacktotten@hotmail.co.uk>
2026-08-19 20:46:41 +0100

    getting started with a leg controller class

---
+++ src/controllers/leg_controller.py
@@ -0,0 +1,5 @@
+
+
+class LegController:
+    def __init__(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,7 @@
+
+from controllers import LegController
+
+if __name__ == "__main__":
+    leg_controller = LegController()
+
+    
