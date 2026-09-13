e16f60710d1ca19f1f3ccc4155ae302871c32cc0
Jack <jacktotten@hotmail.co.uk>
2026-09-13 16:33:18 +0100

    i2c address handling

--- src/controllers/motor_controller.py
+++ src/controllers/motor_controller.py
@@ -4,11 +4,13 @@ import time
 
 class MotorController:
 
-    def __init__(self, i2c):
+    def __init__(self, i2c, address=0x40):
         self.i2c = i2c
+        self.address = address
+        self._instanciate_motor()
 
     def _instanciate_motor(self):
-        self.pca = PCA9685(self.i2c)
+        self.pca = PCA9685(self.i2c, address=self.address)
         self.pca.frequency = 50  # Hz
         self.servo = servo.Servo(self.pca.channels[7])
 
