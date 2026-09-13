a2002a9379d0496962becf99f78958941295de07
Jack <jacktotten@hotmail.co.uk>
2026-09-13 14:49:55 +0100

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
-
+import board
+import busio
 from controllers import LegController
+from controllers import MotorController
 
 
 class Microdot:
     def __init__(self):
+        # Initialize the motor controller class
+        self.i2c = board.I2C()
+        self.motor_controller = MotorController(self.i2c)
+
         # Initialize the microdot with 4 leg controllers
-        self.legs = [LegController(leg_id=i) for i in range(4)]
+        self.legs = [LegController(motor_controller=self.motor_controller, leg_id=i) for i in range(4)]
 
     def get_leg(self, leg_id):
         if 0 <= leg_id < len(self.legs):
